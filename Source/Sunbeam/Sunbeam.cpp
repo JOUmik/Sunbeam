3a746e0bbc0e2a48e323479ea325ec56d0dce5d0
Yan Liu <ly163com01@163.com>
2024-01-16 12:26:50 -0700

    Initialize

---
+++ Source/Sunbeam/Sunbeam.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Sunbeam.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Sunbeam, "Sunbeam" );
+ 
---
+++ Source/Sunbeam/SunbeamGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SunbeamGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ASunbeamGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ASunbeamGameMode();
+};
+
+
+
